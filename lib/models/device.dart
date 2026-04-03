15a3dc62af8893f03b9aae15d3630a04e720b994
MonikaGupta <monikagupta0619@gmail.com>
2026-04-03 23:08:16 +0530

    please commit

---
+++ lib/models/device.dart
@@ -0,0 +1,30 @@
+class Device {
+  final String id;
+  final String name;
+  final bool online;
+  final int lastSeen;
+
+  Device({
+    required this.id,
+    required this.name,
+    required this.online,
+    required this.lastSeen,
+  });
+
+factory Device.fromMap(String id, Map<dynamic , dynamic> map) {
+  return Device (
+  id : id,
+  name : map[ 'name'] ?? 'Unknown'
+  online : map['online'] ?? false,
+  lastSeen : map ['lastSeen'] ?? 0,
+  );
+}
+
+Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'online': online,
+      'lastSeen': lastSeen,
+    };
+  }
+}
